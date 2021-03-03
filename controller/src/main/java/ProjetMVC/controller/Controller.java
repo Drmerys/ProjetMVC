31f2758509cce98b0fc194c6b5cb6289e77ad6b4
Drmerys <bakisdu95@hotmail.fr>
2021-03-03 17:38:48 +0100

    Implementation du controller avec la classe Controller

---
+++ controller/src/main/java/ProjetMVC/controller/Controller.java
@@ -0,0 +1,23 @@
+package ProjetMVC.controller;
+
+import ProjetMVC.model.IModel;
+import ProjetMVC.view.IView;
+
+public class Controller{
+    private final IModel model;
+    private final IView view;
+
+    public Controller(IModel model, IView view){
+        if (model==null)
+            throw new NullPointerException();
+        if (view==null)
+            throw new NullPointerException();
+        this.model = model;
+        this.view = view;
+    }
+
+    public void run()
+    {
+        view.display(model.getMessage());
+    }
+}
